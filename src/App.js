bfe4dad4a559385c88f5f608ff803d6dd3d2fa0c
Travis Bowen <Travisbowen23@gmail.com>
2020-09-01 12:50:13 -0700

    Added info box components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { MenuItem, FormControl, Select } from "@material-ui/core/";
 import "./App.css";
+import InfoBox from "./components/InfoBox";
 
 function App() {
 	const [countries, setCountries] = useState([]);
@@ -29,6 +30,8 @@ function App() {
 	return (
 		<div className='app'>
 			<div className='app__header'>
+				{/* Header */}
+				{/* Title + dropdown */}
 				<h1>COVID-19 TRACKER</h1>
 				<FormControl className='app__dropdown'>
 					<Select variant='outlined' value={country} onChange={onCountryChange}>
@@ -40,12 +43,12 @@ function App() {
 				</FormControl>
 			</div>
 
-			{/* Header */}
-			{/* Title + dropdown */}
-
-			{/* InfoBoxs */}
-			{/* InfoBoxs */}
-			{/* InfoBoxs */}
+			<div className='app__stats'>
+				{/* InfoBoxes */}
+				<InfoBox title='Coronavirus Cases' total={1000} cases={1000} />
+				<InfoBox title='Recovered' total={1000} cases={1000} />
+				<InfoBox title='Deaths' total={1000} cases={1000} />
+			</div>
 
 			{/* Table */}
 			{/* Graph */}
---
+++ src/components/InfoBox.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+function InfoBox({ title, cases, total }) {
+	return (
+		<Card className='infoBox'>
+			<CardContent>
+				{/* Title */}
+				<Typography className='infoBox__title' color='textSecondary'>
+					{title}
+				</Typography>
+				{/* Number of cases */}
+				<h2 className='infoBox__cases' color='textSecondary'>
+					{cases}
+				</h2>
+				{/* Total number of cases */}
+				<Typography className='infoBox__total' color='textSecondary'>
+					{total} Total
+				</Typography>
+			</CardContent>
+		</Card>
+	);
+}
+
+export default InfoBox;
