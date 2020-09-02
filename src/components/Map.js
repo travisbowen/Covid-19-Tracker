8180b24111cdb74d14521885b7e40bfe1c69b0eb
Travis Bowen <Travisbowen23@gmail.com>
2020-09-02 12:29:20 -0700

    Added google maps component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { Map as LeafletMap, TileLayer } from "leaflet";
+import GoogleMapReact from "google-map-react";
+
+const defaultProps = {
+	center: {
+		lat: 59.95,
+		lng: 30.33,
+	},
+	zoom: 3,
+};
 
 function Map() {
-	return <div className='app__map'>I am a map</div>;
+	return (
+		<div className='app__map' style={{ height: "100vh", width: "100%" }}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: "AIzaSyCCdIOocUfrTA7y8r4d1H0ABHbcnnsOzqo" }}
+				defaultCenter={defaultProps.center}
+				defaultZoom={defaultProps.zoom}></GoogleMapReact>
+		</div>
+	);
 }
 
 export default Map;
